a342b32d799d1fabb3a63614b68cbc4fe5cf951d
BenFaruna <benjaminfaruna@gmail.com>
2023-01-26 08:04:59 +0100

    Changed the use of dictionary for ordering items

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,38 +11,30 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """initializing the MRU Cache"""
         super().__init__()
-        self.order = {}
+        self.order = []
 
     def put(self, key, item):
         """adds data to the cache"""
         if key is None or item is None:
             return
 
-        if (len(self.cache_data) == self.MAX_ITEMS) and (
-                key not in self.cache_data):
-            drop_k = self.remove_least_recent()
-            print("DISCARD:", drop_k)
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if key in self.cache_data:
+                del self.cache_data[key]
+                self.order.remove(key)
+            else:
+                del self.cache_data[self.order[self.MAX_ITEMS - 1]]
+                discarded = self.order.pop(self.MAX_ITEMS - 1)
+                print("DISCARD:", discarded)
 
-        self.order[key] = datetime.now()
         self.cache_data[key] = item
+        self.order.append(key)
 
     def get(self, key):
         """gets key value from the cache"""
-        if key is None:
-            return None
-        self.order[key] = datetime.now()
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
 
-    def remove_least_recent(self):
-        """remove most recent entry from cache data"""
-        least_recent = list(self.order.items())[0]
-
-        for key in self.order:
-            if self.order[key] > least_recent[1]:
-                least_recent = (key, self.order[key])
-
-        if (least_recent[0] in self.order) and (
-                least_recent[0] in self.cache_data):
-            del self.order[least_recent[0]]
-            del self.cache_data[least_recent[0]]
-        return least_recent[0]
