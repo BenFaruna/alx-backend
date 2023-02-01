b10210c5ab9a1be6fc8dcfb48e24fce828e74891
BenFaruna <benjaminfaruna@gmail.com>
2023-02-01 01:55:08 +0100

    added flask runner

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,3 +14,13 @@ class Config(object):
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """route for homepage"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
