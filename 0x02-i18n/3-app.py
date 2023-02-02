bb3a6bd7351bbd568c358b922b6173ca064756ab
BenFaruna <benjaminfaruna@gmail.com>
2023-02-02 06:06:47 +0100

    changed _ to gettext

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module for flask application routing"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel, gettext as _
 
 
 class Config(object):
